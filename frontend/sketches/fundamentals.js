1333b003df7477f02d626a23c3ebd76c41381fb3
Yog <yog.mortuum@gmail.com>
2024-10-01 16:53:32 -0600

    enhacements

--- frontend/sketches/fundamentals.js
+++ frontend/sketches/fundamentals.js
@@ -2,37 +2,41 @@ import canvasSketch from 'canvas-sketch';
 
 const settings = {
   dimensions: [800, 800],
-  animate: true,
+  //animate: true,
   canvas: document.getElementById('sketchCanvas') // Use the existing canvas
 };
-
+/**
+ * Function that generates a sketch with random rectangles and inner rectangles based on specified dimensions.
+ * 
+ * @returns {Function} A function that takes a context, width, and height as parameters to draw the sketch.
+ */
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     context.lineWidth = width * 0.01;
 
-    const w = width * 0.1;
-    const h = height * 0.1;
-    const gap = width * 0.03;
-    const ix = width * 0.17;
-    const iy = height * 0.17;
-    const off = width * 0.02;
-
-    let x, y;
+    const rectSize = {
+      width: width * 0.1,
+      height: height * 0.1,
+      gap: width * 0.03,
+      offsetX: width * 0.17,
+      offsetY: height * 0.17,
+      offset: width * 0.02
+    };
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = ix + (w + gap) * i;
-        y = iy + (h + gap) * j;
+        const x = rectSize.offsetX + (rectSize.width + rectSize.gap) * i;
+        const y = rectSize.offsetY + (rectSize.height + rectSize.gap) * j;
 
         context.beginPath();
-        context.rect(x, y, w, h);
+        context.rect(x, y, rectSize.width, rectSize.height);
         context.stroke();
 
         if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + off / 2, y + off / 2, w - off, h - off);
+          context.rect(x + rectSize.offset / 2, y + rectSize.offset / 2, rectSize.width - rectSize.offset, rectSize.height - rectSize.offset);
           context.stroke();
         }
       }
@@ -40,6 +44,22 @@ const sketch = () => {
   };
 };
 
-export function create() {
-  return canvasSketch(sketch, settings);
+
+/**
+ * Delays the execution of a function by a specified amount of time.
+ * 
+ * @param {number} ms - The number of milliseconds to delay the execution.
+ * @returns {Promise} A promise that resolves after the specified delay.
+ */
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Asynchronously creates a sketch after a 1-second delay.
+ * Uses the delay function to wait for 1 second before initializing the canvas sketch with the provided sketch function and settings.
+ */
+export async function create() {
+	await delay(1000);  // 1-second delay
+	canvasSketch(sketch, settings);
 }
